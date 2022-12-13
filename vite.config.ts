914f0ac95250ef4ce300f0507e2d12f24048cb95
Danny Fuhriman <danny.fuhriman@Okta.com>
2022-12-13 07:26:09 -0800

    fix type

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,11 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import svgr from 'vite-plugin-svgr';
 
+import type { UserConfigExport } from 'vite';
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-	const defaultConfig = {
+	const defaultConfig: UserConfigExport = {
 		plugins: [react(), svgr(), tsconfigPaths()],
 	};
 
